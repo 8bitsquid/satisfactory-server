97af28c00f55cbd0ab8613e5229213b316f2d8d2
Will Jones <the8bitsquid@gmail.com>
2021-11-19 18:50:49 -0600

    feat(linux): simple linux systemd service

---
+++ linux/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p /etc/satisfactory.d /opt/satisfactory
+
+curl -o /etc/satisfactory.d/satisfactory  https://raw.githubusercontent.com/8bitsquid/satisfactory-server/main/linux/etc/satisfactory.d/satisfactory
+curl -o /etc/systemd/system/satisfactory.service https://raw.githubusercontent.com/8bitsquid/satisfactory-server/main/linux/etc/systemd/system/satisfactory.service
+
+systemctl daemon-reload
+systemctl enable satisfactory.service
