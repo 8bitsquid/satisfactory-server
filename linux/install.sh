e76dfcbed72c45acb6cd40cb96256155f52d5aa6
Will Jones <the8bitsquid@gmail.com>
2021-11-19 19:19:04 -0600

    fix(linux/install.sh): temparary change raw url to linux branch

--- linux/install.sh
+++ linux/install.sh
@@ -2,8 +2,8 @@
 
 mkdir -p /etc/satisfactory.d /opt/satisfactory
 
-curl -o /etc/satisfactory.d/satisfactory  https://raw.githubusercontent.com/8bitsquid/satisfactory-server/main/linux/etc/satisfactory.d/satisfactory
-curl -o /etc/systemd/system/satisfactory.service https://raw.githubusercontent.com/8bitsquid/satisfactory-server/main/linux/etc/systemd/system/satisfactory.service
+curl -o /etc/satisfactory.d/satisfactory  https://raw.githubusercontent.com/8bitsquid/satisfactory-server/linux-systemd-service/linux/etc/satisfactory.d/satisfactory
+curl -o /etc/systemd/system/satisfactory.service https://raw.githubusercontent.com/8bitsquid/satisfactory-server/linux-systemd-service/linux/etc/systemd/system/satisfactory.service
 
 systemctl daemon-reload
 systemctl enable satisfactory.service
